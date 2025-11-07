cd37deb20f2ec46341d7962f7ede89c41e80b672
Brandon Rubell <brandonrubell@Mac.localdomain>
2025-11-07 18:47:38 -0500

    First lessom on TOP javascript basics

---
+++ javascript.js
@@ -0,0 +1,40 @@
+let name = "John"; 
+let surname = "Doe";
+let age = 11;
+console.log(age); //outputs 11 to the console
+
+age = 54
+
+console.log(age);
+
+console.log(name);
+console.log(surname);
+
+const pi = 3.14;
+
+
+console.log(pi); //output 3.14
+
+
+// Can Reassign let
+// Cannot reassign const
+// var is deprecated but still used
+
+console.log(56 + 47); // prints 103
+console.log(13 + 45 + 67 + 29 + 8 + 10); // prints 172
+
+console.log((4 + 6 + 9) / 77); // prints 0.2467
+
+let a = 10;
+console.log(a);
+
+a = 15;
+console.log(a);
+
+let b = 7 * a;
+console.log(b); // print 165
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage); // Prints 0.77 percent
