f605e62dc3a27e5cc282fca49bfd8480e8c10b7e
Brandon Rubell Desktop <rubelefsky@gmail.com>
2025-11-09 14:59:15 -0500

    More string lessons

--- strings.js
+++ strings.js
@@ -1,4 +1,4 @@
-// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Strings
+// * https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Strings
 
 const string = "The revolution will not be televised.";
 console.log(string);
@@ -34,3 +34,35 @@ console.log(joined); // Hello, how are you?
  * TODO - Todo
  * @param myParam - Parameter fir this method
  */
+
+// * Expressions in strings
+const song = "Fight the youth";
+const score = 9;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${(score / highestScore) * 100}%.`;
+console.log(output);
+
+// * MultiLine Strings
+const newline = `One day you finally knew 
+what you had to do, and began.`;
+console.log(newline);
+
+/*
+One day you finally knew
+what you had to do, and began
+*/
+
+// * Muliline with break character \n
+const newline2 = "One day you finally knew\nwhat you had to do, and began.";
+console.log(newline2);
+
+
+// * Quotes in JS
+const goodQuotes1 = 'She said "I think so"!'
+const goodQuotes2 = `She said "I am not going in there."`;
+console.log(goodQuotes1);
+console.log(goodQuotes2);
+
+// * Escaping characters
+const bigmouth = 'I\'ve got no right to take my place…'; // Escape the character for I've
+console.log(bigmouth);
